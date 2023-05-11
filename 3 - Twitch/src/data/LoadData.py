d408440e963e0c4a406847930fc4fb7d182a6d40
Majid <abdelaouadi@gmail.com>
2023-05-11 10:55:47 +0200

    code the libraries and dataset

---
+++ 3 - Twitch/src/data/LoadData.py
@@ -0,0 +1,12 @@
+# Load libraries needed for this research
+
+import numpy as np
+import pandas as pd
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+# Load the data needed for this research
+
+data = pd.read_csv(
+    r"C:\Users\Eigenaar\Desktop\Python projects\Data\Data-Science\3 - Twitch\data\raw\twitchdata-update.csv"
+)
