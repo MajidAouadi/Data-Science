460d280068249304478563714ce0be053bf4eff8
Majid <abdelaouadi@gmail.com>
2023-05-28 18:07:19 +0200

    add continuous decision tree

--- 3 - Twitch/src/models/train_model.py
+++ 3 - Twitch/src/models/train_model.py
@@ -1,5 +1,4 @@
 # 0.1) Import data from the load data file
-
 import sys
 
 sys.path.append("\\data")
@@ -17,12 +16,12 @@ from sklearn.linear_model import LinearRegression
 # Check data types, names etc.
 data.info()
 
-# Build linear regression model - OLS
+# 1.0) Build linear regression model - OLS
 model = smf.ols("Average_viewers ~ Peak_viewers", data=data)
 model = model.fit()
 model.params
 
-# Evaluate the linear regression with RMSE
+# 1.1) Evaluate the linear regression with RMSE
 
 from sklearn.metrics import mean_squared_error
 from math import sqrt
@@ -30,3 +29,11 @@ from math import sqrt
 sqrt(
     mean_squared_error(data["Average_viewers"], model.predict(data["Peak_viewers"]))
 )  # 6174.5
+
+# 2.0) Build a continuous decision tree
+# Create a seperate dataframe called 'X' which contains all explanatory variables from the 'data' dataframe
+# Except the variable 'Average_viewers' which is the variable we want to predict
+X = pd.DataFrame(data, columns=data.columns)
+X = X.drop(["Average_viewers"], axis=1)
+# Create a seperate dataframe called 'Y' which contains the variable 'Average_viewers' which is the variable we want to predict
+Y = pd.DataFrame(data, columns=["Average_viewers"])
